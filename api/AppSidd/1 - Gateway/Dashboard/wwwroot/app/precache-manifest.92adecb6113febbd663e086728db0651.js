f799bd9e88419b5726fd6dd987a63464a5f9bb71
Douglas Dotto <douglas.dotto@tekann.com.br>
2020-11-19 20:31:17 -0300

    app no azure

---
+++ api/AppSidd/1 - Gateway/Dashboard/wwwroot/app/precache-manifest.92adecb6113febbd663e086728db0651.js
@@ -0,0 +1,142 @@
+self.__precacheManifest = [
+  {
+    "revision": "4e02a4a2c9a5667708c3",
+    "url": "./static/js/app.9ce091a5.chunk.js"
+  },
+  {
+    "revision": "095ea31fb77d30913d5f",
+    "url": "./static/js/runtime~app.30b509be.js"
+  },
+  {
+    "revision": "e8b358439fba0cd69f51",
+    "url": "./static/js/2.92523869.chunk.js"
+  },
+  {
+    "revision": "a37b0c01c0baf1888ca812cc0508f6e2",
+    "url": "././fonts/MaterialIcons.ttf"
+  },
+  {
+    "revision": "3c851d60ad5ef3f2fe43ebd263490d78",
+    "url": "././fonts/MaterialCommunityIcons.ttf"
+  },
+  {
+    "revision": "b2e0fc821c6886fb3940f85a3320003e",
+    "url": "././fonts/Ionicons.ttf"
+  },
+  {
+    "revision": "e20945d7c929279ef7a6f1db184a4470",
+    "url": "././fonts/Foundation.ttf"
+  },
+  {
+    "revision": "744ce60078c17d86006dd0edabcd59a7",
+    "url": "././fonts/Entypo.ttf"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "././fonts/FontAwesome.ttf"
+  },
+  {
+    "revision": "d2285965fe34b05465047401b8595dd0",
+    "url": "././fonts/SimpleLineIcons.ttf"
+  },
+  {
+    "revision": "ca9ce9ff0676a9b04ef0f8a3ad17dd08",
+    "url": "././fonts/Feather.ttf"
+  },
+  {
+    "revision": "3a2ba31570920eeb9b1d217cabe58315",
+    "url": "././fonts/AntDesign.ttf"
+  },
+  {
+    "revision": "b70cea0339374107969eb53e5b1f603f",
+    "url": "././fonts/FontAwesome5_Solid.ttf"
+  },
+  {
+    "revision": "c39278f7abfc798a241551194f55e29f",
+    "url": "././fonts/FontAwesome5_Brands.ttf"
+  },
+  {
+    "revision": "1edd5017f455b09c608e175b1285c06d",
+    "url": "./static/media/date_icon.ec631b15.png"
+  },
+  {
+    "revision": "8b753ae7159e05b15464651a6e23d6c6",
+    "url": "./static/media/info.0dbb46af.png"
+  },
+  {
+    "revision": "361a50eb7fe36e44a4cf29248b6eee3b",
+    "url": "./static/media/login.340b28b4.png"
+  },
+  {
+    "revision": "353bf5e1b4501a945258dc3431d30467",
+    "url": "./static/media/header.b58e71f4.png"
+  },
+  {
+    "revision": "49c48aa37b45ec38400d414a94a791d0",
+    "url": "./static/media/meem1.b04aaf55.png"
+  },
+  {
+    "revision": "e3ae81ca67e24fb60011ab1eea3b181c",
+    "url": "./static/media/moca1.1820130f.png"
+  },
+  {
+    "revision": "f2a30ccf59668950363e0c80fc4b4472",
+    "url": "./static/media/moca2.71e0c6a2.png"
+  },
+  {
+    "revision": "2e29ed75ac05b9690ba5508ac206f961",
+    "url": "./static/media/moca3.d58403b1.png"
+  },
+  {
+    "revision": "7a556852f239e7445122103e28a79393",
+    "url": "./static/media/moca4.c634a15a.png"
+  },
+  {
+    "revision": "cf5bcfe755d15c477d3d53ae40f6508d",
+    "url": "./static/media/moca5.41395295.png"
+  },
+  {
+    "revision": "49f5bd3162b957d93450efb48b58b1e8",
+    "url": "./static/media/avatar.2a7ac26e.png"
+  },
+  {
+    "revision": "b49ae8ab2dbccb02c4d11caaacf09eab",
+    "url": "././fonts/Fontisto.ttf"
+  },
+  {
+    "revision": "6fea585b68f013a4b41e4b1de2f01557",
+    "url": "./index.html"
+  },
+  {
+    "revision": "ec543248d7b23864564429fc03837190",
+    "url": "./serve.json"
+  },
+  {
+    "revision": "2002987acac9deb6803ab4ba381226ba",
+    "url": "./expo-service-worker.js"
+  },
+  {
+    "revision": "f472385f3c88b3edca8350f144fba37c",
+    "url": "./register-service-worker.js"
+  },
+  {
+    "revision": "02e14c79afa4e999e29e9ab7c67fd368",
+    "url": "./static/js/2.92523869.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9894ad9709e144bd60eec5584b9c0d46",
+    "url": "./favicon-16.png"
+  },
+  {
+    "revision": "ea02a35c89eb2826034f3dcfa1f9bb9c",
+    "url": "./favicon-32.png"
+  },
+  {
+    "revision": "ae54b004e9a21155e6cad0e6ff6dff33",
+    "url": "./favicon.ico"
+  },
+  {
+    "revision": "531661f2f12be7f4a322ab222ff6e54d",
+    "url": "./manifest.json"
+  }
+];
